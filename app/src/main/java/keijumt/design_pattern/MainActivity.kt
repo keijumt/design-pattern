aa16994c3737af4544cb10fb12cf4c72a9676c91
keijumt <keijumt@gmail.com>
2018-07-20 19:55:02 +0900

    Add project

---
+++ app/src/main/java/keijumt/design_pattern/MainActivity.kt
@@ -0,0 +1,12 @@
+package keijumt.design_pattern
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
