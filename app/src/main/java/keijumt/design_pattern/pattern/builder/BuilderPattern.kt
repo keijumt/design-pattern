79a985db69769399e552a5386c8ce685b46112ee
keijumt <keijumt@gmail.com>
2018-07-22 20:52:01 +0900

    Add builder pattern of effective java

--- app/src/main/java/keijumt/design_pattern/pattern/builder/BuilderPattern.kt
+++ app/src/main/java/keijumt/design_pattern/pattern/builder/BuilderPattern.kt
@@ -1,5 +1,7 @@
 package keijumt.design_pattern.pattern.builder
 
+import keijumt.design_pattern.entity.Pojo
+
 class BuilderPattern {
 
     fun main() {
@@ -8,7 +10,11 @@ class BuilderPattern {
         val builder = PojoBuilder()
         val director = Director(builder)
         director.construct()
-        val pojo = builder.build()
-        pojo.print()
+        val pojoGof = builder.build()
+        pojoGof.print()
+
+        // EffectiveJavaのBuilderパターン
+        val pojoEffective = Pojo.Builder().addObj1("abc").addObj2("def").addObj3("ghi").build()
+        pojoEffective.print()
     }
 }
