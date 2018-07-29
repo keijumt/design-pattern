185d3a10e6affb3c310954992ca906e57185377c
keijumt <keijumt@gmail.com>
2018-07-29 23:34:15 +0900

    Add mainFragment method using polymorphism

--- app/src/main/java/keijumt/design_pattern/ui/ui/main/BaseActivity.kt
+++ app/src/main/java/keijumt/design_pattern/ui/ui/main/BaseActivity.kt
@@ -1,8 +1,24 @@
 package keijumt.design_pattern.ui.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    /**
+     * Fragmentを指定のViewに貼り付ける。
+     * ポリモーフィズムを使っているため、Fragmentを継承してる
+     * オブジェクトならどのクラスでもaddできる
+     *
+     * @param fragment 貼り付けるFragment
+     * @param containerViewId コンテナViewのID
+     */
+    protected fun addFragment(fragment: Fragment, containerViewId: Int) {
+        supportFragmentManager
+                .beginTransaction()
+                .add(containerViewId, fragment)
+                .commit()
+    }
 
     /**
      * MainFragmentを指定のViewに貼り付ける。
